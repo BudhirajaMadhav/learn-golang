93559bed3f9ddfc423c7c0ae8febaac2763c5388
budhirajamadhav <madhavbudhiraja131@gmail.com>
2021-11-03 00:28:24 +0530

    maps

---
+++ 11map/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in golang")
+
+	languages := make(map[string]string)
+
+	languages["JS"] = "Javascript"
+	languages["PY"] = "Python"
+	languages["RB"] = "Ruby"
+
+	fmt.Println(languages)
+	fmt.Println("JS short for:", languages["JS"])
+
+	// deleting in map
+
+	delete(languages, "RB")
+	fmt.Println(languages)
+
+	// iterating on maps in golang
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v \n", key, value)
+	}
+
+	// comma ok
+	for key, _ := range languages {
+		fmt.Printf("For key %v, value is v \n", key)
+	}
+}
